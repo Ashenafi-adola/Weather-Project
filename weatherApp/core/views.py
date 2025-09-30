0196ee9404a6653e897fdc6166f0ea5b89ad8c95
ashenafi <ashenafiadola05@gmail.com>
2025-09-30 15:05:00 +0300

    modify files

--- weatherApp/core/views.py
+++ weatherApp/core/views.py
@@ -1,3 +1,21 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    city = "Adama"
+    apiKey = "a94b5ab426afd7bceddf051bde36cd48"
+    
+    if request.method == "GET":
+        city = request.GET.get("city")
+        currenturl = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric"
+        baseUrl = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={apiKey}&units=metric"
+    response = requests.get(baseUrl)
+    currentResponse = requests.get(currenturl)
+    currentWeather = currentResponse.json()
+    weather = response.json()
+    
+    context = {
+
+    }
+    return render(request,'core/home.html',context)
